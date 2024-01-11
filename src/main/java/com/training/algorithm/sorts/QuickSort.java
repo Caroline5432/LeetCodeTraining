2a6b0d7c5942c595fec741d2cf03c5561b5a4ea7
caroline <caroline5432@163.com>
2024-01-11 15:33:38 +0800

    O(n)时间复杂度内求无序数组中的第K大元素

--- src/main/java/com/training/algorithm/sorts/QuickSort.java
+++ src/main/java/com/training/algorithm/sorts/QuickSort.java
@@ -15,8 +15,12 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int[] nums = new int[]{4,6,5,3,2,7,1};
-        quickSort(nums, 0,nums.length-1);
+        quickSort(nums, 0,nums.length - 1);
         Arrays.stream(nums).forEach(num -> System.out.print(num + " "));
+        System.out.println();
+
+        int k = findKByQuickSort(nums, 0, nums.length - 1, 4);
+        System.out.println(k);
     }
 
     private static void quickSort(int[] arr, int start, int end) {
@@ -55,6 +59,28 @@ public class QuickSort {
         arr[end] = temp;
 
         return i;
+    }
+
+    /**
+     * 查找 O(n) 时间复杂度内求无序数组中的第 K 大元素。比如，4， 2， 5， 12， 3 这样一组数据，第 3 大元素就是 4。
+     * 我们选择数组区间 A[0...n-1]的最后一个元素 A[n-1]作为 pivot，对数组 A[0...n-1]原地分区，
+     * 这样数组就分成了三部分，A[0...p-1]、A[p]、A[p+1...n-1]。
+     * 如果 p+1=K，那 A[p]就是要求解的元素；如果 K>p+1, 说明第 K 大元素出现在 A[p+1...n-1]区间，
+     * 我们再按照上面的思路递归地在 A[p+1...n-1]这个区间内查找。
+     * 同理，如果 K < p+1,则第K大元素出现在A[0...p-1]。
+     * 递归公式：p = position(0,n)
+     * 终止条件：K = p+1
+     */
+    private static int findKByQuickSort (int[] arr, int start, int end, int k) {
+        int pivot = position(arr, start, end);
+        if (k == pivot + 1) {
+            return arr[pivot];
+        } else if (k < pivot + 1) {
+            return findKByQuickSort(arr, start, pivot - 1, k);
+        } else {
+            return findKByQuickSort(arr, pivot + 1, end, k);
+        }
+
     }
 
 }
